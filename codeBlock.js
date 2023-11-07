b2e82d6c13357f07ecc0bac457bf9de1a80aa120
Wagner Santos <wagtvc@gmail.com>
2023-11-06 22:00:29 -0300

    Update codeBlock.js

--- codeBlock.js
+++ codeBlock.js
@@ -12,10 +12,10 @@ document.querySelectorAll(".myPreTag").forEach((item, index) => {
     } catch (err) {
       var range = document.createRange();
       range.selectNode(document.getElementById("id"+index));
-      window.getSelection().removeAllRanges(); // clear current selection
-      window.getSelection().addRange(range); // to select text
+      window.getSelection().removeAllRanges(); 
+      window.getSelection().addRange(range); 
       document.execCommand("copy");
-      window.getSelection().removeAllRanges();// to deselect
+      window.getSelection().removeAllRanges();
     }
   });
 });
