e65a8e11b9864b364df3182d9ba51c80b907f574
Wagner Santos <wagtvc@gmail.com>
2023-11-06 21:03:13 -0300

    Create codeBlock1.js

---
+++ codeBlock1.js
@@ -0,0 +1,19 @@
+document.querySelectorAll("iframe").forEach((item, index) => {
+   item.setAttribute("allow", "clipboard-write clipboard-read");
+});
+
+document.querySelectorAll(".myPreTag").forEach((item, index) => {
+  item.setAttribute("data-value", "Copy");
+  item.addEventListener("click", async (event) => {
+    if (!navigator.clipboard) {
+      // Clipboard API not available
+      return;
+    }
+    try {
+      await navigator.clipboard.writeText(event.target.innerText);
+      item.setAttribute("data-value", "Copied");
+    } catch (err) {
+      console.error("Failed to copy!", err);
+    }
+  });
+});
